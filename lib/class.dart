74811501ad30c229333b9ac856559caf2665f9cb
Vivek060454 <98967443+Vivek060454@users.noreply.github.com>
2023-02-20 11:56:48 +0530

    Add files via upload

---
+++ lib/class.dart
@@ -0,0 +1,25 @@
+class Userdetails {
+  Userdetails({
+    this.id,
+    this.usertitle,
+    this.userdescription,
+    this.firstname,
+    this.lastname,
+    this.email,
+    this.gender,
+    this.education,
+    this.title,
+    this.description,
+  });
+
+  String? id;
+  String? usertitle;
+  String? userdescription;
+  String? firstname;
+  String? lastname;
+  String? email;
+  String? gender;
+  String? education;
+  String? title;
+  String? description;
+}
